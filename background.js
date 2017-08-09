231de4cd73c46d00b74f3575c99cccab8bdb47aa
yinhanchun <yinhanchun@lvmama.com>
2017-08-09 10:26:10 +0800

    readme

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null,{file:"assets/js/jquery-1.11.3.js"});
   chrome.tabs.executeScript(null,{file:"assets/js/clipboard.min.js"});
-  chrome.tabs.executeScript(null,{file:"assets/js/getCrawler.js"});
-  // chrome.tabs.executeScript(null,{file:"assets/js/crawler.js"});
+  chrome.tabs.executeScript(null,{file:"assets/js/crawler.js"});
+  // chrome.tabs.executeScript(null,{file:"assets/js/getCrawler.js"});
 });
